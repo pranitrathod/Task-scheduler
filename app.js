134283e9f752d67f04f98c91f722e2e37d2b4240
pranitrathod <pranitrathodsocial@gmail.com>
2023-11-15 13:40:51 +0530

    codeed for adding tasks in card!

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const card=document.querySelector('ul');
 const NewButton=document.getElementById("btn-new");
 
+const div=document.getElementById("cd");
 NewButton.addEventListener('click',()=>{
     // const ul=document.createElement('ul');
-    const div=document.createElement('div');
-    div.innerHTML=`<h1>DONE</h1>`;
-    card.append(div);
-    console.log('clicked!');
+    const outInput=document.getElementsByClassName("form-input")[0];
+    const input=document.createElement('input');
+    const daysLabel=document.createElement("label");
+    const dates=new Date();
+    const shortingDate={weekday:'short',month:'short',day:'numeric',year:'numeric'};
+    const formattedDate=dates.toLocaleDateString('en-US',shortingDate);
+
+    daysLabel.innerText=formattedDate;
+    const tasklabel=document.createElement('label');
+    tasklabel.innerText=outInput.value;
+    input.type="checkbox";
+    div.className="cd";
+    div.append(daysLabel);
+    div.append(tasklabel);
+    div.append(input);
+
+
 })
