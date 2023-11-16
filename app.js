0726cc5f78ed16d550e9c6982947235315881413
pranitrathod <pranitrathodsocial@gmail.com>
2023-11-16 17:11:06 +0530

    designed card!

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
-const card=document.querySelector('ul');
+const card=document.getElementsByClassName('container')[0];
 const NewButton=document.getElementById("btn-new");
+const addButton=document.getElementById('add-btn');
+// const div=document.getElementById("cd");
+addButton.addEventListener('click',()=>{
 
-const div=document.getElementById("cd");
-NewButton.addEventListener('click',()=>{
-    // const ul=document.createElement('ul');
-    const outInput=document.getElementsByClassName("form-input")[0];
-    const input=document.createElement('input');
-    const daysLabel=document.createElement("label");
-    const dates=new Date();
-    const shortingDate={weekday:'short',month:'short',day:'numeric',year:'numeric'};
-    const formattedDate=dates.toLocaleDateString('en-US',shortingDate);
+    // const div1=document.createElement("div");
+    // const div2=document.createElement('div');
+    // const div3=document.createElement('div');
 
-    daysLabel.innerText=formattedDate;
-    const tasklabel=document.createElement('label');
-    tasklabel.innerText=outInput.value;
-    input.type="checkbox";
-    div.className="cd";
-    div.append(daysLabel);
-    div.append(tasklabel);
-    div.append(input);
-
-
+    // div1.classList="box";
+    // div2.classList="box";
+    // div3.classList="box";
+    // card.append(div1);
+    // card.append(div2);
+    // card.append(div3);
 })
+//     addButton.addEventListener('click',()=>{
+//     // const ul=document.createElement('ul');
+//     const outInput=document.getElementsByClassName("form-input")[0];
+//     const input=document.createElement('input');
+//     const daysLabel=document.createElement("label");
+//     const dates=new Date();
+//     const shortingDate={weekday:'short',month:'short',day:'numeric',year:'numeric'};
+//     const formattedDate=dates.toLocaleDateString('en-US',shortingDate);
+//
+//     daysLabel.innerText=formattedDate;
+//     const tasklabel=document.createElement('label');
+//     tasklabel.innerText=outInput.value;
+//     input.type="checkbox";
+//     div.className="cd";
+//     div.append(daysLabel);
+//     div.append(tasklabel);
+//     div.append(input);
+// })
+//
+// const outInput=document.getElementsByClassName("form-input")[0];
+// const input=document.createElement('input');
+// const daysLabel=document.createElement("label");
+// const dates=new Date();
+// const shortingDate={weekday:'short',month:'short',day:'numeric',year:'numeric'};
+// const formattedDate=dates.toLocaleDateString('en-US',shortingDate);
+// daysLabel.innerText=formattedDate;
+// const tasklabel=document.createElement('label');
+// tasklabel.innerText=outInput.value;
+// input.type="checkbox";
