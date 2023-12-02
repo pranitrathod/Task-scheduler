1c0412411e03d4ee0aa6b77267dd52ea6287b965
pranitrathod <pranitrathodsocial@gmail.com>
2023-12-02 18:01:25 +0530

    getting rid of some repeatation code!

--- app.js
+++ app.js
@@ -1,39 +1,68 @@
-const card=document.getElementsByClassName('container')[0];
 const NewButton=document.getElementById("btn-new");
 const addButton=document.getElementById('add-btn');
-// const div=document.getElementById("cd");
-addButton.addEventListener('click',()=>{
+const inputTask=document.getElementsByClassName("form-input")[0];
 
-    // const div1=document.createElement("div");
-    // const div2=document.createElement('div');
-    // const div3=document.createElement('div');
+NewButton.addEventListener('click',()=> {
+//Card1
+  const container1 = document.createElement("div");
+  const card1= document.createElement("div");
+  const cardContent1 = document.createElement("div");
+  container1.className = "container";
+  card1.className = "card";
+  cardContent1.className = "card-content";
+  cardContent1.innerHTML = `
+  <h3>To be done!</h3>
+  <label>Javascript</label>
+  <input type="checkbox"></input>
+`;
 
-    // div1.classList="box";
-    // div2.classList="box";
-    // div3.classList="box";
-    // card.append(div1);
-    // card.append(div2);
-    // card.append(div3);
+  //Card2
+
+  const container2 = document.createElement("div");
+  const card2= document.createElement("div");
+  const cardContent2 = document.createElement("div");
+  container2.className = "container";
+  card2.className = "card";
+  cardContent2.className = "card-content";
+  cardContent2.innerHTML = `
+  <h3>On going task!</h3>
+  <label>Javascript</label>
+  <input type="checkbox"></input>
+`;
+
+  card1.append(cardContent1);
+  container1.append(card1);
+  document.body.append(container1);
+
+  card2.append(cardContent2);
+  container2.append(card2);
+  document.body.append(container2);
+
+
+
+
 })
-//     addButton.addEventListener('click',()=>{
-//     // const ul=document.createElement('ul');
-//     const outInput=document.getElementsByClassName("form-input")[0];
-//     const input=document.createElement('input');
-//     const daysLabel=document.createElement("label");
-//     const dates=new Date();
-//     const shortingDate={weekday:'short',month:'short',day:'numeric',year:'numeric'};
-//     const formattedDate=dates.toLocaleDateString('en-US',shortingDate);
-//
-//     daysLabel.innerText=formattedDate;
-//     const tasklabel=document.createElement('label');
-//     tasklabel.innerText=outInput.value;
-//     input.type="checkbox";
-//     div.className="cd";
-//     div.append(daysLabel);
-//     div.append(tasklabel);
-//     div.append(input);
-// })
-//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+addButton.addEventListener('click',()=> {
+
+
 // const outInput=document.getElementsByClassName("form-input")[0];
 // const input=document.createElement('input');
 // const daysLabel=document.createElement("label");
@@ -44,3 +73,6 @@ addButton.addEventListener('click',()=>{
 // const tasklabel=document.createElement('label');
 // tasklabel.innerText=outInput.value;
 // input.type="checkbox";
+
+
+})
